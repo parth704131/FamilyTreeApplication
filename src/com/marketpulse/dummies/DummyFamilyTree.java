d4e8562b4ea7bec38e40b7c4fcdd634b78f703f9
parth704131 <parth.sarkheliya18@gmail.com>
2020-09-01 14:12:32 +0530

    clean main method add dummy class for dummy data

---
+++ src/com/marketpulse/dummies/DummyFamilyTree.java
@@ -0,0 +1,49 @@
+package com.marketpulse.dummies;
+
+import com.marketpulse.models.Gender;
+import com.marketpulse.models.Person;
+
+public class DummyFamilyTree {
+
+    public static Person getDummyFamilyTree() {
+
+        Person john = new Person("john", "sakes", Gender.MALE);
+        Person maryArden = new Person("mary", "arden", Gender.FEMALE);
+
+        Person joan = new Person("joan", "sakes", Gender.FEMALE, john);
+        Person marget = new Person("marget", "sakes", Gender.FEMALE, joan);
+        Person gillbert = new Person("gillbert", "sakes", Gender.MALE, john);
+        Person joann = new Person("joann", "sakes", Gender.FEMALE, john);
+        Person anne = new Person("anne", "sakes", Gender.FEMALE, john);
+        Person richard = new Person("richard", "sakes", Gender.MALE, john);
+        Person edmund = new Person("edmund", "sakes", Gender.MALE, john);
+        Person willims = new Person("wilims", "sakes", Gender.MALE, john);
+
+        john.getChildrens().add(joan);
+        john.getChildrens().add(marget);
+        john.getChildrens().add(gillbert);
+        john.getChildrens().add(joann);
+        john.getChildrens().add(anne);
+        john.getChildrens().add(richard);
+        john.getChildrens().add(edmund);
+        john.getChildrens().add(willims);
+
+        Person susanna = new Person("susanna", "sakes", Gender.FEMALE, willims);
+        Person hamnet = new Person("hamnet", "sakes", Gender.MALE, willims);
+        Person judith = new Person("judith", "sakes", Gender.FEMALE, willims);
+
+        willims.getChildrens().add(susanna);
+        willims.getChildrens().add(hamnet);
+        willims.getChildrens().add(judith);
+
+        Person elizabeth = new Person("elizabeth", "sakes", Gender.FEMALE);
+        Person sakespeare = new Person("sakespeare", "sakes", Gender.MALE);
+        Person thomas = new Person("thomas", "sakes", Gender.MALE);
+
+        susanna.getChildrens().add(elizabeth);
+        judith.getChildrens().add(sakespeare);
+        judith.getChildrens().add(thomas);
+
+        return john;
+    }
+}
